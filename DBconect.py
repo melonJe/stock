7cc79b0e6a375a070fed0c3fb9e23604d379506b
melonJe <jmayermj@gmail.com>
2022-08-09 22:37:53 +0900

    update

--- DBconect.py
+++ DBconect.py
@@ -1,21 +1,23 @@
-from bs4 import BeautifulSoup
-import pandas as ps
+from time import strptime
+from bs4 import BeautifulSoup as bs
+import requests
+import pandas as pd
 import pymysql
-from datetime import *
-from stock_API import *
+import datetime as dt
+import stock_API as API
 import secret
 
-ps.set_option('display.max_rows', None)
-ps.set_option('display.max_columns', None)
-ps.set_option('display.width', None)
-ps.set_option('display.max_colwidth', None)
+pd.set_option('display.max_rows', None)
+pd.set_option('display.max_columns', None)
+pd.set_option('display.width', None)
+pd.set_option('display.max_colwidth', None)
 
 
 class DBconect:
     def __init__(self):
         self.conn = pymysql.connect(
             user=secret.DBUSER,
-            passwd=secret.DBpass,
+            passwd=secret.DBPASS,
             host=secret.HOST,
             port=secret.PORT,
             db=secret.DBNAME,
@@ -36,7 +38,7 @@ class DBconect:
             if srtnCd:
                 select_sql = 'SELECT srtnCd,basDt,hipr,clpr,lopr,trqu from stock_price_data where srtnCd=%s and basDt<(select basDt from latest_time_data) ORDER BY basDt DESC LIMIT 245'
                 cursor.execute(select_sql, srtnCd)
-                data = ps.concat([ps.DataFrame(cursor.fetchall(), columns=('srtnCd', 'basDt', 'hipr', 'clpr', 'lopr', 'trqu')).sort_values(by='basDt'), data])
+                data = pd.concat([pd.DataFrame(cursor.fetchall(), columns=('srtnCd', 'basDt', 'hipr', 'clpr', 'lopr', 'trqu')).sort_values(by='basDt'), data])
             data['ema5'] = data['clpr'].ewm(span=5, min_periods=5).mean()
             data['ema20'] = data['clpr'].ewm(span=20, min_periods=20).mean()
             data['ema60'] = data['clpr'].ewm(span=60, min_periods=60).mean()
@@ -63,16 +65,16 @@ class DBconect:
             cursor.execute(select_sql)
             stocks = [x[0] for x in cursor.fetchall()]
             insert_sql = "REPLACE INTO stock_price_data(srtnCd,basDt,hipr,clpr,lopr,trqu,ema5,ema20,ema60,ema120,ema240,PMF,NMF,perb,MFI,slow_d) VALUES(%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s)"
-            answer = ps.DataFrame()
+            answer = pd.DataFrame()
             for srtnCd in stocks:
                 url = 'https://finance.naver.com/item/sise.naver?code=' + srtnCd
                 html = requests.get(url).text
-                soup = BeautifulSoup(html, 'html.parser')
+                soup = bs(html, 'html.parser')
                 tags = soup.findAll('dd')
-                data = ps.DataFrame([[tags[2].text.split(' ')[1], datetime.strptime(soup.findAll('em', attrs={'class': 'date'})[0].text.split(' ')[0], '%Y.%m.%d').strftime('%Y-%m-%d'), tags[6].text.split(' ')[1].replace(',', ''), tags[3].text.split(' ')[1].replace(',', ''),
+                data = pd.DataFrame([[tags[2].text.split(' ')[1], strptime(soup.findAll('em', attrs={'class': 'date'})[0].text.split(' ')[0], '%Y.%m.%d'), tags[6].text.split(' ')[1].replace(',', ''), tags[3].text.split(' ')[1].replace(',', ''),
                                       tags[8].text.split(' ')[1].replace(',', ''), tags[10].text.split(' ')[1].replace(',', '')]], columns=['srtnCd', 'basDt', 'hipr', 'clpr', 'lopr', 'trqu']).astype({'clpr': 'int64', 'trqu': 'int64'})
                 data = self.stock_criteria(data, srtnCd)
-                answer = ps.concat([answer, data[-1:]])
+                answer = pd.concat([answer, data[-1:]])
             if not answer.empty:
                 answer = answer.fillna(0)
                 cursor.executemany(insert_sql, answer.values.tolist())
@@ -88,17 +90,17 @@ class DBconect:
             insert_sql = "REPLACE INTO stock_price_data(srtnCd,basDt,hipr,clpr,lopr,trqu,ema5,ema20,ema60,ema120,ema240,PMF,NMF,perb,MFI,slow_d) VALUES(%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s)"
             delete_sql = 'delete from saved_srtnCd where srtnCd=%s'
             date_sql = "UPDATE latest_time_data SET basDt=%s"
-            answer = ps.DataFrame()
+            answer = pd.DataFrame()
             for srtnCd in stocks:
                 cursor.execute(select_sql, srtnCd)
-                data = get_stock_price(beginBasDt=last_date.strftime("%Y%m%d"), endBasDt=date.today().strftime("%Y%m%d"), likeSrtnCd=srtnCd)
-                l = len(data)
+                data = API.get_stock_price(beginBasDt=last_date.strftime("%Y%m%d"), endBasDt=dt.date.today().strftime("%Y%m%d"), likeSrtnCd=srtnCd)
+                dataLen = len(data)
                 if data.empty:
                     cursor.execute(delete_sql, srtnCd)
                     self.conn.commit()
                     continue
                 data = self.stock_criteria(data, srtnCd)
-                answer = ps.concat([answer, data[-l:]])
+                answer = pd.concat([answer, data[-dataLen:]])
             if not answer.empty:
                 answer = answer.fillna(0)
                 cursor.executemany(insert_sql, answer.values.tolist())
@@ -109,7 +111,7 @@ class DBconect:
         with self.conn.cursor() as cursor:
             sql = "REPLACE INTO stock_price_data(srtnCd,basDt,hipr,clpr,lopr,trqu,ema5,ema20,ema60,ema120,ema240,PMF,NMF,perb,MFI,slow_d) VALUES(%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s)"
             for x in stocks:
-                data = ps.DataFrame(get_stock_price(beginBasDt=(date.today() - timedelta(days=1000)).strftime("%Y%m%d"), endBasDt=date.today().strftime("%Y%m%d"), numOfRows=1000, likeSrtnCd=x)).astype({'clpr': 'int64', 'trqu': 'int64'})
+                data = pd.DataFrame(API.get_stock_price(beginBasDt=(dt.date.today() - dt.timedelta(days=1000)).strftime("%Y%m%d"), endBasDt=dt.date.today().strftime("%Y%m%d"), numOfRows=1000, likeSrtnCd=x)).astype({'clpr': 'int64', 'trqu': 'int64'})
                 data = self.stock_criteria(data)
                 if not data.empty:
                     data = data[['srtnCd', 'basDt', 'hipr', 'clpr', 'lopr', 'trqu', 'ema5', 'ema20', 'ema60', 'ema120', 'ema240', 'PMF', 'NMF', 'perb', 'MFI', 'slow_d']]
@@ -118,17 +120,21 @@ class DBconect:
                     cursor.executemany(sql, data.values.tolist())
                 self.conn.commit()
 
+    def get_buy_0_or_1(self):
+        with self.conn.cursor() as cursor:
+            select_sql = 'SELECT srtnCd FROM saved_srtnCd WHERE buy=0 or buy=1'
+            cursor.execute(select_sql)
+            return [x[0] for x in cursor.fetchall()]
+
     def bollinger(self):
         buy = set()
         sell = set()
         with self.conn.cursor() as cursor:
-            select_sql = 'SELECT srtnCd FROM saved_srtnCd WHERE buy=0 or buy=1'
-            cursor.execute(select_sql)
-            stocks = [x[0] for x in cursor.fetchall()]
+            stocks = self.get_buy_0_or_1()
             select_sql = 'SELECT a.*,saved_srtnCd.itmsNm,saved_srtnCd.buy FROM (select srtnCd,basDt,hipr,clpr,lopr,trqu,perb,MFI from stock_price_data where srtnCd=%s) as a JOIN saved_srtnCd ON a.srtnCd=saved_srtnCd.srtnCd ORDER BY basDt DESC LIMIT 1'
             for x in stocks:
                 cursor.execute(select_sql, x)
-                data = ps.DataFrame(cursor.fetchall(), columns=['srtnCd', 'basDt', 'hipr', 'clpr', 'lopr', 'trqu', 'perb', 'MFI', 'itmsNm', 'buy'])
+                data = pd.DataFrame(cursor.fetchall(), columns=['srtnCd', 'basDt', 'hipr', 'clpr', 'lopr', 'trqu', 'perb', 'MFI', 'itmsNm', 'buy'])
                 if data['buy'].values[-1] == 0:
                     if 0.8 < data['perb'].values[-1] and data['MFI'].values[-1] > 80:
                         buy.add(data['itmsNm'].values[-1])
@@ -141,13 +147,11 @@ class DBconect:
         buy = set()
         sell = set()
         with self.conn.cursor() as cursor:
-            select_sql = 'SELECT srtnCd FROM saved_srtnCd WHERE buy=0 or buy=1'
-            cursor.execute(select_sql)
-            stocks = [x[0] for x in cursor.fetchall()]
+            stocks = stocks = self.get_buy_0_or_1()
             select_sql = 'SELECT a.*,saved_srtnCd.itmsNm,saved_srtnCd.buy FROM (select srtnCd,basDt,hipr,clpr,lopr,trqu,ema120,slow_d from stock_price_data where srtnCd=%s) as a JOIN saved_srtnCd ON a.srtnCd=saved_srtnCd.srtnCd ORDER BY basDt DESC LIMIT 2'
             for x in stocks:
                 cursor.execute(select_sql, x)
-                data = ps.DataFrame(cursor.fetchall(), columns=['srtnCd', 'basDt', 'hipr', 'clpr', 'lopr', 'trqu', 'ema120', 'slow_d', 'itmsNm', 'buy']).sort_values(by='basDt')
+                data = pd.DataFrame(cursor.fetchall(), columns=['srtnCd', 'basDt', 'hipr', 'clpr', 'lopr', 'trqu', 'ema120', 'slow_d', 'itmsNm', 'buy']).sort_values(by='basDt')
                 if data['buy'].values[-1] == 0:
                     if data['ema120'].values[-2] < data['ema120'].values[-1] and data['slow_d'].values[-1] < 15:
                         buy.add(data['itmsNm'].values[-1])
@@ -160,13 +164,11 @@ class DBconect:
         buy = set()
         # sell = set()
         with self.conn.cursor() as cursor:
-            select_sql = 'SELECT srtnCd FROM saved_srtnCd WHERE buy=0 or buy=1'
-            cursor.execute(select_sql)
-            stocks = [x[0] for x in cursor.fetchall()]
+            stocks = stocks = self.get_buy_0_or_1()
             select_sql = 'SELECT a.*,saved_srtnCd.itmsNm,saved_srtnCd.buy FROM (select srtnCd,basDt,hipr,clpr,lopr,trqu,ema120 from stock_price_data where srtnCd=%s) as a JOIN saved_srtnCd ON a.srtnCd=saved_srtnCd.srtnCd ORDER BY basDt DESC LIMIT 30'
             for x in stocks:
                 cursor.execute(select_sql, x)
-                data = ps.DataFrame(cursor.fetchall(), columns=['srtnCd', 'basDt', 'hipr', 'clpr', 'lopr', 'trqu', 'ema120', 'itmsNm', 'buy']).sort_values(by='basDt')
+                data = pd.DataFrame(cursor.fetchall(), columns=['srtnCd', 'basDt', 'hipr', 'clpr', 'lopr', 'trqu', 'ema120', 'itmsNm', 'buy']).sort_values(by='basDt')
                 data = data.sort_values(by='basDt', ascending=True)
                 if data['buy'].values[-1] == 0:
                     if data['lopr'].values[-1] <= data['ema120'].values[-1] <= data['hipr'].values[-1]:
