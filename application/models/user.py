cc1020f29ebabcfed14eb34955fdc9e7bb85257b
SangEun Jeong <sejeong@mv-w.com>
2023-02-17 18:47:25 +0900

    20230217

--- application/models/user.py
+++ application/models/user.py
@@ -1,36 +1,17 @@
-from typing import Optional
-from sqlalchemy import ForeignKey
-from sqlalchemy import String
-from sqlalchemy.orm import DeclarativeBase
-from sqlalchemy.orm import Mapped
-from sqlalchemy.orm import mapped_column
-from sqlalchemy.orm import relationship
+from django.db import models
 
-class Base(DeclarativeBase):
-    pass
+class user(models.Model):
+    email = models.CharField
 
-class User(Base):
-    __tablename__ = "user_account"
+    def __str__(self):
+        return self.email
 
-    id: Mapped[int] = mapped_column(primary_key=True)
-    name: Mapped[str] = mapped_column(String(30))
-    fullname: Mapped[Optional[str]]
 
-    addresses: Mapped[list["Address"]] = relationship(
-        back_populates="user", cascade="all, delete-orphan"
-    )
+class stock(models.Model):
+    symbol = models.CharField
+    name = models.CharField
+    unit = models.CharField
 
-    def __repr__(self) -> str:
-        return f"User(id={self.id!r}, name={self.name!r}, fullname={self.fullname!r})"
+    def __str__(self):
+        return self.name
 
-class Address(Base):
-    __tablename__ = "address"
-
-    id: Mapped[int] = mapped_column(primary_key=True)
-    email_address: Mapped[str]
-    user_id: Mapped[int] = mapped_column(ForeignKey("user_account.id"))
-
-    user: Mapped["User"] = relationship(back_populates="addresses")
-
-    def __repr__(self) -> str:
-        return f"Address(id={self.id!r}, email_address={self.email_address!r})"
