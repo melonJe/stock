860cb5ca439d6ec8226646a6985c0c744813db89
melonJe <jayermj@gmail.com>
2023-02-25 21:38:08 +0900

    pony orm using

--- application/__init__.py
+++
@@ -1,4 +0,0 @@
-from application.api.moon import moon
-from application.api.sun import sun
-from application.moonphase import MoonPhase
-from application.solstice import Solstice
--- application/solstice.py
+++
@@ -1,6 +0,0 @@
-class Solstice:
-    def __init__(self, equinox, autumnal_equinox, summer_solstice, winter_solstice):
-        self.equinox = equinox
-        self.autumnal_equinox = autumnal_equinox
-        self.summer_solstice = summer_solstice
-        self.winter_solstice = winter_solstice
