df6172c347b1294b9f2a792add2f697b53e4ee9a
majaro-asus <jmayermj@gmail.com>
2023-05-15 20:32:39 +0900

    add_stock_price_1day add_stock_price_1week

--- main.py
+++ main.py
@@ -1,9 +1,7 @@
 import datetime
-import time
 import schedule
-
-from app.database.db_connect import *
 import FinanceDataReader
+from app.database.db_connect import *
 
 
 def add_stock():
@@ -13,31 +11,50 @@ def add_stock():
     insert_list = []
     for item in df_krx.to_dict('records'):
         insert_list.append({'symbol': item['Code'], 'name': item['Name']})
-    Stock.insert_many(insert_list).on_conflict('REPLACE').execute()
-
-    # .execute_query('insert ignore into from stock ', )
-    # db = DBHelper.getInstance()
-    # Execute a query and retrieve the results
-    # result = db.execute_query("SELECT * FROM mytable")
-    # Print the results
-    # print(result)
+    Stock.insert_many(insert_list).on_conflict_ignore().execute()
 
 
-def add_stock_price():
-    df_krx = FinanceDataReader.DataReader('068270', '2023-05-12')
-    print(df_krx)
+def add_stock_price_1day():
+    now = datetime.datetime.now()
+    if now.weekday() in (5, 6):
+        return
+    insert_list = []
+    for stock in Stock.select(Stock.symbol):
+        df_krx = FinanceDataReader.DataReader(stock.symbol, now.strftime('%Y-%m-%d'), now.strftime('%Y-%m-%d'))
+        for idx, item in df_krx.iterrows():
+            insert_list.append(
+                {'symbol': stock.symbol, 'date': idx, 'open': item['Open'], 'high': item['High'],
+                 'close': item['Close'], 'low': item['Low']})
+    StockPrice.insert_many(insert_list).on_conflict_ignore().execute()
+
+
+def add_stock_price_1week():
+    now = datetime.datetime.now()
+    if now.weekday() in (5, 6):
+        return
+    for stock in Stock.select(Stock.symbol):
+        df_krx = FinanceDataReader.DataReader(stock.symbol, (now - datetime.timedelta(days=7)).strftime('%Y-%m-%d'),
+                                              now.strftime('%Y-%m-%d'))
+        insert_list = []
+        for idx, item in df_krx.iterrows():
+            insert_list.append(
+                {'symbol': stock.symbol, 'date': idx, 'open': item['Open'], 'high': item['High'],
+                 'close': item['Close'], 'low': item['Low']})
+        StockPrice.insert_many(insert_list).on_conflict_ignore().execute()
 
 
 if __name__ == "__main__":
     DBConnect().db.create_tables([User, Stock, StockPrice, StockBuy, StockSubscription])
-    add_stock()
-    schedule.every().days.do(add_stock)
-    # schedule.every().hour.do(job)
-    # schedule.every().day.at("10:30").do(job)
-    # schedule.every().monday.do(job)
-    # schedule.every().wednesday.at("13:15").do(job)
-    # schedule.every().minute.at(":17").do(job)
+    schedule.every().monday.do(add_stock)
+    schedule.every().monday.do(add_stock_price_1day)
+    schedule.every().sunday.do(add_stock_price_1week)
     while True:
         schedule.run_pending()
         time.sleep(1)
         break
+
+# schedule.every().hour.do(job)
+# schedule.every().day.at("10:30").do(job)
+# schedule.every().monday.do(job)
+# schedule.every().wednesday.at("13:15").do(job)
+# schedule.every().minute.at(":17").do(job)
