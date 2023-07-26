9ef6566d195fee2cc51d91271b65e033659f3e51
melon_zenbook <jmayermj@gmail.com>
2023-07-27 00:06:36 +0900

    pool_pre_ping

--- main.py
+++ main.py
@@ -10,10 +10,10 @@ if __name__ == "__main__":
     print(f"DB_NAME {config.DB_NAME}")
     print(f"DB_USER {config.DB_USER}")
     print(f"DB_PASS {config.DB_PASS}")
-    # schedule.every().sunday.do(schedule_helper.add_stock_price_1week)
+    schedule.every().sunday.do(schedule_helper.add_stock_price_1week)
     schedule.every().monday.do(schedule_helper.add_stock)
     schedule.every().day.at("22:00").do(schedule_helper.add_stock_price_1day)
-    schedule.every().day.at("07:00").do(schedule_helper.bollinger_band)
+    schedule.every().day.at("08:30").do(schedule_helper.bollinger_band)
     while True:
         schedule.run_pending()
         time.sleep(0.9)
