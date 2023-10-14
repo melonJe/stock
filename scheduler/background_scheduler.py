fd2ee7aaea0184504034ede3fc169a43ffab3b59
melon_zenbook <jmayermj@gmail.com>
2023-10-15 03:04:21 +0900

    data['ma200']

--- scheduler/background_scheduler.py
+++ scheduler/background_scheduler.py
@@ -174,7 +174,7 @@ def bollinger_band(account: KoreaInvestment):
             data = pd.DataFrame(StockPrice.objects.filter(date__range=[datetime.now() - timedelta(days=365), datetime.now()], symbol=stock.symbol).order_by('date').values())
             if data.empty:
                 continue
-            data['ma200'] = data['close'].rolling(window=200).mean()  # 추가한 코드
+            # data['ma200'] = data['close'].rolling(window=200).mean()  # 추가한 코드
             data['ma150'] = data['close'].rolling(window=150).mean()  # 추가한 코드
             data['ma50'] = data['close'].rolling(window=50).mean()  # 추가한 코드
             data['ma20'] = data['close'].rolling(window=20).mean()
@@ -190,7 +190,7 @@ def bollinger_band(account: KoreaInvestment):
             # data['decision'] = np.where(data['PB'] < 0.2 and data['MFI10'] < 20, '매수')
             # data['decision'] = np.where(data['PB'] > 0.8 and data['MFI10'] > 80, '매도')
 
-            if not (data.iloc[-1]['ma200'] < data.iloc[-1]['ma150'] < data.iloc[-1]['ma50']):  # 추가한 코드
+            if not (data.iloc[-1]['ma150'] < data.iloc[-1]['ma50']):  # 추가한 코드
                 continue  # 추가한 코드
 
             if data.iloc[-1]['PB'] < 0.2 and data.iloc[-1]['MFI10'] < 20:
@@ -201,7 +201,6 @@ def bollinger_band(account: KoreaInvestment):
             data = pd.DataFrame(StockPrice.objects.filter(date__range=[datetime.now() - timedelta(days=365), datetime.now()], symbol=stock['pdno']).order_by('date').values())
             if data.empty:
                 continue
-            data['ma200'] = data['close'].rolling(window=200).mean()  # 추가한 코드
             data['ma150'] = data['close'].rolling(window=150).mean()  # 추가한 코드
             data['ma50'] = data['close'].rolling(window=50).mean()  # 추가한 코드
             data['ma20'] = data['close'].rolling(window=20).mean()
@@ -214,7 +213,7 @@ def bollinger_band(account: KoreaInvestment):
             data['NMF'] = np.where(data['close'].diff(1) < 0, data['TP'] * data['volume'], 0)
             data['MFR'] = data['PMF'].rolling(window=10).mean() / data['NMF'].rolling(window=10).mean()
             data['MFI10'] = 100 - 100 / (1 + data['MFR'])
-            if not (data.iloc[-1]['ma200'] < data.iloc[-1]['ma150'] < data.iloc[-1]['ma50']):  # 추가한 코드
+            if not (data.iloc[-1]['ma150'] < data.iloc[-1]['ma50']):  # 추가한 코드
                 decision['sell'].add(stock['pdno'])  # 추가한 코드
                 continue  # 추가한 코드
             if data.iloc[-1]['PB'] > 0.8 and data.iloc[-1]['MFI10'] > 80:
