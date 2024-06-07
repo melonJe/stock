030b1274af89289faafc7a06d3e636443e3250bf
i7-14700K <cabs0814@naver.com>
2024-06-07 18:34:20 +0900

    update_sell_queue

--- stock/service/trading.py
+++ stock/service/trading.py
@@ -202,9 +202,10 @@ def update_sell_queue(ki_api: KoreaInvestmentAPI, email: Account):
 
         if trade_type == "02":
             volumes_and_prices = [
-                (int(volume * 0.5), price_refine(int(price * 1.1))),
+                (int(volume * 0.4), price_refine(int(price * 1.15))),
                 (int(volume * 0.3), price_refine(int(price * 1.2))),
-                (volume - int(volume * 0.5) - int(volume * 0.3), price_refine(int(price * 1.5)))
+                (int(volume * 0.2), price_refine(int(price * 1.3))),
+                (volume - int(volume * 0.4) - int(volume * 0.3) - int(volume * 0.2), price_refine(int(price * 1.6)))
             ]
 
             for vol, prc in volumes_and_prices:
@@ -249,9 +250,10 @@ def update_sell_queue(ki_api: KoreaInvestmentAPI, email: Account):
             avg_price = float(stock.pchs_avg_pric)
 
             volumes_and_prices = [
-                (int(additional_volume * 0.5), price_refine(int(avg_price * 1.1))),
+                (int(additional_volume * 0.4), price_refine(int(avg_price * 1.15))),
                 (int(additional_volume * 0.3), price_refine(int(avg_price * 1.2))),
-                (additional_volume - int(additional_volume * 0.5) - int(additional_volume * 0.3), price_refine(int(avg_price * 1.5)))
+                (int(additional_volume * 0.2), price_refine(int(avg_price * 1.3))),
+                (additional_volume - int(additional_volume * 0.4) - int(additional_volume * 0.3) - int(additional_volume * 0.2), price_refine(int(avg_price * 1.6)))
             ]
 
             for vol, prc in volumes_and_prices:
