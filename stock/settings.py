e31ee3e469e7ff57e8943d7d72ee2040f99f0e4f
i7-14700K <cabs0814@naver.com>
2025-01-07 09:39:22 +0900

    db 수정

--- stock/settings.py
+++ stock/settings.py
@@ -73,10 +73,7 @@ WSGI_APPLICATION = 'stock.wsgi.application'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    },
+    'default': {},
     "stock_db": {
         "ENGINE": "django.db.backends.postgresql_psycopg2",
         "NAME": setting_env.DB_NAME,
